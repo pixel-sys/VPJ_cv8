47bf56a9f4962c51fcfd1bd99d9c0ecb719da121
Xaver pixel Zak <59127040+pixel-sys@users.noreply.github.com>
2022-04-14 10:21:06 +0200

    Add project files.

---
+++ VPJ_cv8/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace VPJ_pxl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.suborToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.farbaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zmenitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otvoritToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ulozitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.koniecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pismoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pozadieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.suborToolStripMenuItem,
+            this.fontToolStripMenuItem,
+            this.farbaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // suborToolStripMenuItem
+            // 
+            this.suborToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otvoritToolStripMenuItem,
+            this.ulozitToolStripMenuItem,
+            this.koniecToolStripMenuItem});
+            this.suborToolStripMenuItem.Name = "suborToolStripMenuItem";
+            this.suborToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.suborToolStripMenuItem.Text = "Subor";
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zmenitToolStripMenuItem});
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.fontToolStripMenuItem.Text = "Font";
+            // 
+            // farbaToolStripMenuItem
+            // 
+            this.farbaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pismoToolStripMenuItem,
+            this.pozadieToolStripMenuItem});
+            this.farbaToolStripMenuItem.Name = "farbaToolStripMenuItem";
+            this.farbaToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.farbaToolStripMenuItem.Text = "Farba";
+            // 
+            // zmenitToolStripMenuItem
+            // 
+            this.zmenitToolStripMenuItem.Name = "zmenitToolStripMenuItem";
+            this.zmenitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.zmenitToolStripMenuItem.Text = "zmenit";
+            this.zmenitToolStripMenuItem.Click += new System.EventHandler(this.zmenitToolStripMenuItem_Click);
+            // 
+            // otvoritToolStripMenuItem
+            // 
+            this.otvoritToolStripMenuItem.Name = "otvoritToolStripMenuItem";
+            this.otvoritToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.otvoritToolStripMenuItem.Text = "otvorit";
+            this.otvoritToolStripMenuItem.Click += new System.EventHandler(this.otvoritToolStripMenuItem_Click);
+            // 
+            // ulozitToolStripMenuItem
+            // 
+            this.ulozitToolStripMenuItem.Name = "ulozitToolStripMenuItem";
+            this.ulozitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ulozitToolStripMenuItem.Text = "ulozit";
+            this.ulozitToolStripMenuItem.Click += new System.EventHandler(this.ulozitToolStripMenuItem_Click);
+            // 
+            // koniecToolStripMenuItem
+            // 
+            this.koniecToolStripMenuItem.Name = "koniecToolStripMenuItem";
+            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.koniecToolStripMenuItem.Text = "koniec";
+            this.koniecToolStripMenuItem.Click += new System.EventHandler(this.koniecToolStripMenuItem_Click);
+            // 
+            // pismoToolStripMenuItem
+            // 
+            this.pismoToolStripMenuItem.Name = "pismoToolStripMenuItem";
+            this.pismoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pismoToolStripMenuItem.Text = "pismo";
+            this.pismoToolStripMenuItem.Click += new System.EventHandler(this.pismoToolStripMenuItem_Click);
+            // 
+            // pozadieToolStripMenuItem
+            // 
+            this.pozadieToolStripMenuItem.Name = "pozadieToolStripMenuItem";
+            this.pozadieToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pozadieToolStripMenuItem.Text = "pozadie";
+            this.pozadieToolStripMenuItem.Click += new System.EventHandler(this.pozadieToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 426);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 24);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 18);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 389);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Lorem ipsum";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem suborToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otvoritToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ulozitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem koniecToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zmenitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem farbaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pismoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pozadieToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ VPJ_cv8/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VPJ_pxl
+{
+    public partial class Form1 : Form
+    {
+
+        String fontName;
+        float fontSize;
+        Color fontColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void otvoritToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "txt (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.InitialDirectory = @"C:\Users\piXel\Desktop";
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {   
+
+                textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void ulozitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt (*.txt)|*.txt";
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.InitialDirectory = @"C:\Users\piXel\Desktop";
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+            }
+        }
+
+        private void zmenitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            //fontDialog.ShowDialog();
+
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                fontName = fontDialog.Font.Name;
+                fontSize = fontDialog.Font.Size;
+                
+                toolStripStatusLabel1.Text = (fontName + ": " + fontSize);
+
+                textBox1.Font = new Font(fontName, fontSize);
+            }
+        }
+
+        private void pismoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.ForeColor = colorDialog1.Color;
+            }
+
+        }
+
+        private void koniecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pozadieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
